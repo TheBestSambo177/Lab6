eddd6daf5e31e577f677d9143e8b2e5ce84a64fe
Sambo177 <83565347+TheBestSambo177@users.noreply.github.com>
2022-09-14 11:30:49 +1200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+//alert("Hello");
+//Data: assume we have a list of top 5 movies - a list of JAVASCRIPT Objects
+let topMovies = [{id: 0, title: "The Shawshank Redemption", year: 1994, image_url: "MEDIA/movie0.jpg"},
+				 {id: 1, title: "The Godfather ", year: 1972, image_url: "MEDIA/movie1.jpg"},
+				 {id: 2, title: "The Dark Knight", year: 2008, image_url: "MEDIA/movie2.jpg"},
+			     {id: 3, title: "12 Angry Men", year: 1957, image_url: "MEDIA/movie3.jpg"},
+			     {id: 4, title: " Schindler\'s List", year: 1993, image_url: "MEDIA/movie4.jpg"},
+				];
+
+//------------------------------------------------------------------------------------------------------
+//Question 1: Look at the above list of movie "Javascript" Objects, 
+//            do you see the different between "JSON" object and "Javascript" Object?
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+//Load JSON File stored on web server at http://danieldangs.com/itwd6408/json/faqs.json
